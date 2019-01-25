b80762865097a097b375b9e64d103587efef8d71
Charles <charles.bordet@gmail.com>
2019-01-25 14:32:26 +0100

    adapt ann with coursework

--- ann/ann.py
+++ ann/ann.py
@@ -22,9 +22,68 @@ y = y.values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# ANN
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 13))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+# Part 3 - Making predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Predicting a single new observation
+"""Predict if the customer with the following informations will leave the bank:
+Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40
+Tenure: 3
+Balance: 60000
+Number of Products: 2
+Has Credit Card: Yes
+Is Active Member: Yes
+Estimated Salary: 50000"""
+Xnew = pd.DataFrame(data={
+        'CreditScore': [600], 
+        'Geography': ['France'], 
+        'Gender': ['Male'],
+        'Age': [40],
+        'Tenure': [3],
+        'Balance': [60000],
+        'NumOfProducts': [2],
+        'HasCrCard': [1],
+        'IsActiveMember': [1],
+        'EstimatedSalary': [50000]})
+Xnew = preprocess.transform(Xnew)
+new_prediction = classifier.predict(Xnew)
+new_prediction = (new_prediction > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Evaluate
 def build_classifier(optimizer='adam'):
     classifier = Sequential()
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 13))
@@ -32,15 +91,6 @@ def build_classifier(optimizer='adam'):
     classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
     classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
     return classifier
-
-# Predict
-ann = build_classifier()
-ann.fit(X_train, y_train, batch_size = 10, epochs = 100)
-y_pred = ann.predict(X_test)
-y_pred = (y_pred > 0.5)
-
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
 
 # Evaluate
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -54,7 +104,7 @@ variance = accuracies.std()
 # Tune
 from sklearn.model_selection import GridSearchCV
 classifier = KerasClassifier(build_fn = build_classifier)
-parameters = {'batch_size': [10, 25, 32],
+parameters = {'batch_size': [25, 32],
               'epochs': [100, 500],
               'optimizer': ['adam', 'rmsprop']}
 grid_search = GridSearchCV(estimator = classifier,
